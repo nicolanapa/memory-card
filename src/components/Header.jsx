25bbc96532fc4684869a9c2b256914a5c3503f57
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-14 19:46:29 +0200

    Added Scores() with some basic HTML code and Header() to contain Title and Scores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,3 +3,23 @@ import React from "react";
 function Title() {
 	return <h1>Memory Card</h1>;
 }
+
+function Scores() {
+	return (
+		<div className="scores">
+			<p>Score: {}</p>
+			<p>Best Score: {}</p>
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<div>
+			<Title />
+			<Scores />
+		</div>
+	);
+}
+
+export { Header };
