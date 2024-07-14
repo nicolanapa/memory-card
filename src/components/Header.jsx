9486e83054939f688df5a2909489fda395c5a1d6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-14 20:15:24 +0200

    Added some comments about a Solution if the current one with Scores() doesn't work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,10 @@ function Title() {
 	return <h1>Memory Card</h1>;
 }
 
+// If doesn't work well then move Scores to Game()
+// And move it to Game.jsx
+// To be able to use Game() Props
+
 function Scores() {
 	return (
 		<div className="scores">
