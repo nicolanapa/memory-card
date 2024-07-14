c142dad3912d111e3716ec33d6003a5b8f52c0c9
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-14 19:57:36 +0200

    Imported Game and included it in the Render

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style.css";
 import { Header } from "./components/Header.jsx";
+import { Game } from "./components/Game.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Header />
+		<Game />
 	</React.StrictMode>
 );
-
