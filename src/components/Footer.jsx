07a842ba434aa9a291f67f55112bc932142fcf20
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-14 20:09:31 +0200

    Added Footer() which has a Copyright section about the brand I use here

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer>
+			<h3>Copyright</h3>
+			<div>
+				<p>Pokémon and Pokémon character names are trademarks of Nintendo.</p>
+				<p>
+					The API used to take Informations and Images about Pokémons is <a href="https://pokeapi.co/">PokéAPI</a>
+				</p>
+			</div>
+		</footer>
+	);
+}
+
+export { Footer };
