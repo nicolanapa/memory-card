accc0ffda1075628eeb35ff127e1ba31c7c0ccc7
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-14 20:10:12 +0200

    Imported Footer() and used it at the bottom of the Rendering

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./styles/style.css";
 import { Header } from "./components/Header.jsx";
 import { Game } from "./components/Game.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Header />
 		<Game />
+		<Footer />
 	</React.StrictMode>
 );
