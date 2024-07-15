b4b37ed2cb19262e5694821f25cfa45dd23d5e0e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-15 16:09:31 +0200

    Added CardChooser() with fetches the Pokémons randomly and returns an Array with them. Added allCards State in Game()

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Card() {}
 
 function CardMover() {}
+
+async function CardChooser() {
+	let cards = [];
+	let allPokemons = 1302;
+	let allID = [];
+	for (let i = 0; i < 2; i++) {
+		allID.push(Math.floor(Math.random() * allPokemons));
+
+		let tempPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + allID[i] + "/");
+		tempPokemon = await tempPokemon.json();
+		cards.push(tempPokemon);
+	}
+	console.log(cards);
+	console.log(allID);
+
+	return cards;
+}
 
 function Scores() {
 	return (
@@ -14,6 +31,8 @@ function Scores() {
 }
 
 function Game() {
+	const [allCards, SetAllCards] = useState(CardChooser);
+
 	return (
 		<main>
 			<Scores />
