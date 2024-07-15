2c81700e782f8e14ff3137136c492043a482cc32
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-15 17:12:36 +0200

    Added htmlCards in CardChooser() so it will immediately push the needed Cards and added allCards and htmlCards Stated in Game alongside useEffect

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/game.css";
 
-function Card() {}
+function Card({ pokemonName, /*pokemonDescription,*/ pokemonSvg, key }) {
+	return (
+		<div className="card" key={key}>
+			<img src={pokemonSvg} alt={pokemonName} />
+			<h2>{pokemonName}</h2>
+			{/*<p>{pokemonDescription}</p>*/}
+		</div>
+	);
+}
 
 function CardMover() {}
 
@@ -8,7 +17,7 @@ async function CardChooser() {
 	let cards = [];
 	let allPokemons = 1302;
 	let allID = [];
-	for (let i = 0; i < 2; i++) {
+	for (let i = 0; i < 6; i++) {
 		allID.push(Math.floor(Math.random() * allPokemons));
 
 		let tempPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + allID[i] + "/");
@@ -18,7 +27,17 @@ async function CardChooser() {
 	console.log(cards);
 	console.log(allID);
 
-	return cards;
+	let htmlCards = [];
+	for (let i = 0; i < 6; i++) {
+		htmlCards.push(
+			<Card
+				pokemonName={cards[i].name}
+				/*pokemonDescription={}*/ pokemonSvg={cards[i].sprites.other.dream_world.front_default}
+				key={cards[i].id}
+			/>
+		);
+	}
+	return { cards, htmlCards };
 }
 
 function Scores() {
@@ -31,11 +50,22 @@ function Scores() {
 }
 
 function Game() {
-	const [allCards, SetAllCards] = useState(CardChooser);
+	const [allCards, setAllCards] = useState();
+	const [htmlCards, setHtmlCards] = useState();
+
+	useEffect(() => {
+		async function fetchData() {
+			let tempAllCards = await CardChooser();
+			setAllCards(tempAllCards.cards);
+			setHtmlCards(tempAllCards.htmlCards);
+		}
+		fetchData();
+	});
 
 	return (
 		<main>
 			<Scores />
+			<section className="card-container">{htmlCards}</section>
 		</main>
 	);
 }
