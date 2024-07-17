cbf49c259b3b25dcbda4809ffce0cf641f506522
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-17 15:50:26 +0200

    Added setHasClicked and position Parameters to be able to modify the hasClicked State from a Card Component. Added replaceAt() Function to modify easily the hasClicked array

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { CardMover } from "./CardMover";
 
-function Card({ pokemonName, /*pokemonDescription,*/ pokemonSvg, hasClicked }) {
+function Card({ pokemonName, /*pokemonDescription,*/ pokemonSvg, hasClicked, setHasClicked, position }) {
+	function replaceAt(array, index, value) {
+		array[index] = value;
+		console.log(array);
+		return array;
+	}
+
+	function settingClicked() {
+		if (hasClicked[position] === 0) {
+			setHasClicked(replaceAt(hasClicked, position, 1));
+			CardMover();
+		} else {
+			// 0 1
+			setHasClicked(replaceAt(hasClicked, position, 0));
+			CardMover();
+		}
+
+		console.log("INTERNAL POSITION " + position, hasClicked);
+	}
+
 	return (
-		<div className="card">
+		<div className="card" onClick={settingClicked}>
 			<h2>{pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h2>
 			{/*<p>{pokemonDescription}</p>*/}
 
