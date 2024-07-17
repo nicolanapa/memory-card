3d6d7ef47a53f98d97a3e802e8a7b705f5b712c5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-17 17:18:10 +0200

    Changed score name to actual for better referencing

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Scores({ score }) {
 	return (
 		<div className="scores">
-			<p>Score: {score.score}</p>
+			<p>Score: {score.actual}</p>
 			<p>Best Score: {score.bestScore}</p>
 		</div>
 	);
