62f777ded47b4e3f46419e843997dcf8476c0708
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-17 14:56:21 +0200

    Moved the htmlCards code section from CardChooser() to its own Component

--- src/components/CardChooser.jsx
+++ src/components/CardChooser.jsx
@@ -44,41 +44,7 @@ async function CardChooser() {
 	console.log(cards);
 	console.log(allID);
 
-	let htmlCards = [];
-	for (let i = 0; i < 10; i++) {
-		if (cards[i].sprites.other.dream_world.front_default === null) {
-			if (cards[i]["sprites"]["other"]["official-artwork"]["front_default"] !== null) {
-				htmlCards.push(
-					<Card
-						pokemonName={cards[i].name}
-						/*pokemonDescription={}*/ pokemonSvg={cards[i]["sprites"]["other"]["official-artwork"]["front_default"]}
-						key={cards[i].id}
-					/>
-				);
-			} else {
-				// If the high resolution Image doesn't exist then choose this
-				// Highly unlikely
-				// Pixel-variation
-
-				htmlCards.push(
-					<Card
-						pokemonName={cards[i].name}
-						/*pokemonDescription={}*/ pokemonSvg={cards[i].sprites.front_default}
-						key={cards[i].id}
-					/>
-				);
-			}
-		} else {
-			htmlCards.push(
-				<Card
-					pokemonName={cards[i].name}
-					/*pokemonDescription={}*/ pokemonSvg={cards[i].sprites.other.dream_world.front_default}
-					key={cards[i].id}
-				/>
-			);
-		}
-	}
-	return { cards, htmlCards };
+	return cards;
 }
 
 export { CardChooser };
---
+++ src/components/CardHtml.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Card } from "./Card";
+
+function CardHtml(cards) {
+	let htmlCards = [];
+	console.log("Test", cards);
+	for (let i = 0; i < 10; i++) {
+		if (cards[i].sprites.other.dream_world.front_default === null) {
+			if (cards[i]["sprites"]["other"]["official-artwork"]["front_default"] !== null) {
+				htmlCards.push(
+					<Card
+						pokemonName={cards[i].name}
+						/*pokemonDescription={}*/ pokemonSvg={cards[i]["sprites"]["other"]["official-artwork"]["front_default"]}
+						key={cards[i].id}
+					/>
+				);
+			} else {
+				// If the high resolution Image doesn't exist then choose this
+				// Highly unlikely
+				// Pixel-variation
+
+				htmlCards.push(
+					<Card
+						pokemonName={cards[i].name}
+						/*pokemonDescription={}*/ pokemonSvg={cards[i].sprites.front_default}
+						key={cards[i].id}
+					/>
+				);
+			}
+		} else {
+			htmlCards.push(
+				<Card
+					pokemonName={cards[i].name}
+					/*pokemonDescription={}*/ pokemonSvg={cards[i].sprites.other.dream_world.front_default}
+					key={cards[i].id}
+				/>
+			);
+		}
+	}
+
+	return htmlCards;
+}
+
+export { CardHtml };
