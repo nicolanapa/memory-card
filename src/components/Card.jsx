5b482ca104e8eef07d33fdb2b421828f58220e9e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-17 15:10:29 +0200

    Added the parameteter 'hasClicked', not using it yet

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Card({ pokemonName, /*pokemonDescription,*/ pokemonSvg }) {
+function Card({ pokemonName, /*pokemonDescription,*/ pokemonSvg, hasClicked }) {
 	return (
 		<div className="card">
 			<h2>{pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h2>
