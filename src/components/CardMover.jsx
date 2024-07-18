49c31676f259f456462724c2adcc09bc0a8e43a5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-18 14:57:25 +0200

    Removed the allCards parameter from the Function

--- src/components/CardMover.jsx
+++ src/components/CardMover.jsx
@@ -1,19 +1,16 @@
-function CardMover(allCards, hasClicked) {
-	console.log("BEFORE MOVING", allCards, hasClicked);
+function CardMover(hasClicked) {
+	console.log("BEFORE MOVING", hasClicked);
 
 	for (let i = 0; i < 10; i++) {
 		let numberToRandomize = Math.floor(Math.random() * 10);
-		let tempCard = allCards[i];
 		let tempHasClicked = hasClicked[i];
-		allCards[i] = allCards[numberToRandomize];
-		allCards[numberToRandomize] = tempCard;
 		hasClicked[i] = hasClicked[numberToRandomize];
 		hasClicked[numberToRandomize] = tempHasClicked;
 	}
 
-	console.log("AFTER MOVING", allCards, hasClicked);
+	console.log("AFTER MOVING", hasClicked);
 
-	return { allCards, hasClicked };
+	return hasClicked;
 }
 
 export { CardMover };
