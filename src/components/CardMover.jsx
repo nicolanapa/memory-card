f23973ad671ea50835dbdd8e90870c671ebe3a95
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-18 14:22:00 +0200

    Added a for loop which randomizes allCards and hasClicked and returns an Object of these two Arrays

--- src/components/CardMover.jsx
+++ src/components/CardMover.jsx
@@ -1,3 +1,19 @@
-function CardMover() {}
+function CardMover(allCards, hasClicked) {
+	console.log("BEFORE MOVING", allCards, hasClicked);
+
+	for (let i = 0; i < 10; i++) {
+		let numberToRandomize = Math.floor(Math.random() * 10);
+		let tempCard = allCards[i];
+		let tempHasClicked = hasClicked[i];
+		allCards[i] = allCards[numberToRandomize];
+		allCards[numberToRandomize] = tempCard;
+		hasClicked[i] = hasClicked[numberToRandomize];
+		hasClicked[numberToRandomize] = tempHasClicked;
+	}
+
+	console.log("AFTER MOVING", allCards, hasClicked);
+
+	return { allCards, hasClicked };
+}
 
 export { CardMover };
